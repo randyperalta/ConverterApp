a532a436a9416dbb97f2c8a7a95826fc5e81854c
Randy Peralta <randyperalta@outlook.com>
2023-04-11 22:13:08 -0400

    Firts version

---
+++ ConverterApp/ConverterApp/ConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterAppApp.swift
+//  ConverterApp
+//
+//  Created by Randy Peralta on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
